523880210dcc253fe17f17da1495835ea8186a6a
n3o59hf <n3o59hf@c603ebf0-0c9f-11de-a021-8b77bb4f2096>
2009-03-10 01:12:26 +0000

    0.0.5 Remake communications with shell. Added some features (path to swap file, progress indicator). Added threads. Changed visual apperance.

--- trunk/src/lv/n3o/swapper/Swapper.java
+++ trunk/src/lv/n3o/swapper/Swapper.java
@@ -1,9 +1,11 @@
 package lv.n3o.swapper;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,22 +15,103 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Swapper extends Activity implements OnClickListener {
 
-	/** variables */
-	Button swapon_32;
-	Button swapoff;
-	Button set_swappiness;
-	Button set_swappiness_10;
-	Button get_swappiness;
-	Button get_info;
-	EditText swappiness;
-	TextView log;
-	Button startsettings;
-	SharedPreferences settings;
+public class Swapper extends Activity implements OnClickListener, Runnable {
 
-	int swapSize;
-	String swapPlace;
+	ProgressDialog		dia;
+	Button				get_info;
+	Button				get_swappiness;
+	TextView			log;
+	Thread				progress;
+	Button				set_swappiness;
+	Button				set_swappiness_10;
+	SharedPreferences	settings;
+	Button				startsettings;
+	SuCommander			su;
+
+	Button				swapoff;
+
+	Button				swapon_32;
+	EditText			swappiness;
+
+	String				swapPlace;
+
+	int					swapSize;
+
+	@Override
+	public void onClick(View arg0) {
+		if (arg0.equals(startsettings)) {
+			Intent i = new Intent(this, SwapperSettings.class);
+			startActivity(i);
+			finish();
+			return;
+		}
+		dia = new ProgressDialog(this);
+		dia.setCancelable(false);
+		dia.setMessage("Please wait...");
+		dia.setTitle("Executing command");
+		log.setText("");
+		try {
+			if (arg0.equals(swapon_32)) {
+				log.append("Starting... (It can take long time)\n");
+				su.exec("dd if=/dev/zero of=" + swapPlace + " bs=1M count="
+						+ swapSize + " && mkswap " + swapPlace + " && swapon "
+						+ swapPlace);
+				progress = new Thread(this);
+				progress.start();
+				dia.show();
+				// log.append(su.get_output("swap_creation_end"));
+			} else if (arg0.equals(swapoff)) {
+				log.append("Stopping...\n");
+				su.exec("swapoff " + swapPlace + " && rm " + swapPlace);
+				progress = new Thread(this);
+				progress.start();
+				dia.show();
+			} else if (arg0.equals(get_swappiness)) {
+				swappiness.setText(su.exec_o("cat /proc/sys/vm/swappiness"));
+			} else if (arg0.equals(set_swappiness)) {
+				su.exec("echo " + swappiness.getText().toString()
+						+ " > /proc/sys/vm/swappiness");
+			} else if (arg0.equals(set_swappiness_10)) {
+				su.exec("echo 10 > /proc/sys/vm/swappiness");
+			} else if (arg0.equals(get_info)) {
+				log.append("Swappiness: ");
+				log.append(su.exec_o("cat /proc/sys/vm/swappiness"));
+				// structure
+				// arraylists
+				// total, used, free
+				try {
+					String[] free = su.exec_o("free").split("\n");
+					ArrayList<String> mem = new ArrayList<String>();
+					for (String s : free[1].split(" ")) {
+						s.replace(" ", "");
+						if (s.length() > 0) {
+							mem.add(s);
+						}
+					}
+					log.append("Memory:\t" + mem.get(1) + " KB \n\tUsed:\t"
+							+ mem.get(2) + "\n\tfree:\t" + mem.get(3) + "\n");
+
+					mem = new ArrayList<String>();
+					for (String s : free[2].split(" ")) {
+						s.replace(" ", "");
+						if (s.length() > 0) {
+							mem.add(s);
+						}
+					}
+					log.append("Swap: \t" + mem.get(1) + " KB \n\tUsed:\t"
+							+ mem.get(2) + "\n\tfree\t" + mem.get(3) + "\n");
+				} catch (ArrayIndexOutOfBoundsException e) {
+					log.append("Reading error. Please try one more time.");
+				}
+			} else {
+				return;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			log.append(e.getMessage());
+		}
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -55,84 +138,30 @@ public class Swapper extends Activity implements OnClickListener {
 		startsettings = (Button) findViewById(R.id.StartSettings);
 		startsettings.setOnClickListener(this);
 		swappiness = (EditText) findViewById(R.id.Swappiness);
-	}
-
-	@Override
-	public void onClick(View arg0) {
-		if (arg0.equals(startsettings)) {
-			Intent i = new Intent(this, SwapperSettings.class);
-			startActivity(i);
-			this.finish();
-			return;
-		}
-		SuCommander su;
 		try {
 			su = new SuCommander();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-		log.setText("");
-		try {
-			su.get_output(); // cleanup
-			if (arg0.equals(swapon_32)) {
-				log.append("Starting... (It can take long time)\n");
-				su
-						.exec("dd if=/dev/zero of=" + swapPlace +" bs=1M count="
-								+ swapSize
-								+ " && mkswap " + swapPlace +  " && swapon " + swapPlace +"; echo swap_creation_end");
-				log.append(su.get_output("swap_creation_end"));
-				log.append("OK");
-			} else if (arg0.equals(swapoff)) {
-				log.append("Stopping...\n");
-				su
-						.exec("swapoff " + swapPlace +  " && rm " + swapPlace +"  ; echo swap_deactivation_end");
-				log.append(su.get_output("swap_deactivation_end"));
-				log.append("OK");
-			} else if (arg0.equals(get_swappiness)) {
-				swappiness.setText(su.exec_o("cat /proc/sys/vm/swappiness"));
-			} else if (arg0.equals(set_swappiness)) {
-				su.exec("echo " + swappiness.getText().toString()
-						+ " > /proc/sys/vm/swappiness");
-			} else if (arg0.equals(set_swappiness_10)) {
-				su.exec("echo 10 > /proc/sys/vm/swappiness");
-			} else if (arg0.equals(get_info)) {
-				log.append("Swappiness: ");
-				log.append(su.exec_o("cat /proc/sys/vm/swappiness"));
-				log.append("\n");
-				// structure
-				// arraylists
-				// total, used, free
-				try {
-					String[] free = su.exec_o("free").split("\n");
-					ArrayList<String> mem = new ArrayList<String>();
-					for (String s : free[1].split(" ")) {
-						s.replace(" ", "");
-						if (s.length() > 0)
-							mem.add(s);
-					}
-					log.append("Memory:\t" + mem.get(1) + " KB \n\tUsed:\t"
-							+ mem.get(2) + "\n\tfree:\t" + mem.get(3) + "\n");
-
-					mem = new ArrayList<String>();
-					for (String s : free[2].split(" ")) {
-						s.replace(" ", "");
-						if (s.length() > 0)
-							mem.add(s);
-					}
-					log.append("Swap: \t" + mem.get(1) + " KB \n\tUsed:\t"
-							+ mem.get(2) + "\n\tfree\t" + mem.get(3) + "\n");
-				} catch (ArrayIndexOutOfBoundsException e) {
-					// TODO: handle exception
-					log.append("Reading error. Please try one more time.");
-				}
-			} else
-				return;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			log.append(e.getMessage());
+			e.printStackTrace();
 		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		while (!su.isReady()) {
+			// dia.incrementProgressBy(10);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+		}
+		dia.dismiss();
+		// log.setText(su.getOutput());
 	}
 }
