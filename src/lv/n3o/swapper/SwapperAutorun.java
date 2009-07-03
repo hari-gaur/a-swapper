c99db9dc435a6619276ca7ecbee0a0aaedcb97ad
n3o59hf <n3o59hf@c603ebf0-0c9f-11de-a021-8b77bb4f2096>
2009-07-03 12:50:45 +0000

    Swapper 0.1.1 sync

---
+++ src/lv/n3o/swapper/SwapperAutorun.java
@@ -0,0 +1,27 @@
+package lv.n3o.swapper;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+
+public class SwapperAutorun extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		Boolean autorun = settings.getBoolean("startup", true);
+		if (!autorun)
+			return;
+		Toast.makeText(context, "Swapper starting...", 100).show();
+		SwapperCommands sc = new SwapperCommands(context);
+		sc.swappiness();
+		sc.swapon();
+	}
+
+}
